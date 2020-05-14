b716850f7b579a594d2ba01dfcaa6b1a2db2f3fb
jazzyjackson <colten.jackson@lookalive.software>
2020-05-14 04:21:47 -0500

    converted whitespace to 4 space indents

--- elementary.js
+++ elementary.js
@@ -1,7 +1,7 @@
 const Type = require('@lookalive/type')
 const empty_elements = [
-	"!DOCTYPE html",
-	"area","base","br","col","embed","hr","img","input","link","meta","param","source","track","wbr"
+    "!DOCTYPE html",
+    "area","base","br","col","embed","hr","img","input","link","meta","param","source","track","wbr"
 ]
 /**
  * @param {object | array} template the source template in the form {tagname: {attrs}} or list thereof
@@ -10,54 +10,54 @@ const empty_elements = [
 module.exports = dispatch
 
 function dispatch(template){
-	switch(Type.from(template).identity){
-		case Array:
-			return template.map(dispatch).join('')
-		case Object:
+    switch(Type.from(template).identity){
+        case Array:
+            return template.map(dispatch).join('')
+        case Object:
             return Object.entries(template).shift().shift() // dig into the tagname of {tagname:{}}
                 .match(/^style$/i)
                     ? makeStyleNode(template)
                     : makeElementNode(template)
         default:
             return String(template)
-	}
+    }
 }
 
 function makeStyleNode(template){
     let CSSSelectorSets = Object.entries(template).pop().pop()
-	return interpolate('style', [makeCSSSelectorSet(CSSSelectorSets)])
+    return interpolate('style', [makeCSSSelectorSet(CSSSelectorSets)])
 }
 
 function makeElementNode(template){
-	let [[tagName, attributePairs]] = Object.entries(template)
+    let [[tagName, attributePairs]] = Object.entries(template)
 
-	if(tagName == '!'){
+    if(tagName == '!'){
         return process.env.NOSCRIPT
             ? `\n<!-- ${JSON.stringify(attributePairs)} -->\n`
             : dispatch({script:[`console.warn(JSON.parse(${JSON.stringify(attributePairs)}))`]})
-	} else if(Type.isArray(attributePairs)){
+    } else if(Type.isArray(attributePairs)){
         // Arrays are used to skip attributes on the outerhtml and give child nodes directly, text nodes are no problem.
-		return interpolate(tagName, dispatch(attributePairs))
-	} else {
+        return interpolate(tagName, dispatch(attributePairs))
+    } else {
         let innerHTML = new Array
         let outerHTML = new Array
 
-		for(var [attributeName, attributeValue] of Object.entries(attributePairs)){
-			switch(attributeName.toLowerCase()){
-	 			case 'childnodes':
-	 				innerHTML.push(dispatch(attributeValue)) // dispatch joins arrays into strings after mapping
-	 				break
-	 			case 'style':
-	 				attributeValue = makeCSSRuleValuePairs(attributeValue)
-	 				outerHTML.push(makeHTMLAttribute(attributeName, attributeValue))
-	 				break
-	 			default:
-	 				outerHTML.push(makeHTMLAttribute(attributeName, attributeValue))
-	 		}
+        for(var [attributeName, attributeValue] of Object.entries(attributePairs)){
+            switch(attributeName.toLowerCase()){
+                case 'childnodes':
+                    innerHTML.push(dispatch(attributeValue)) // dispatch joins arrays into strings after mapping
+                    break
+                case 'style':
+                    attributeValue = makeCSSRuleValuePairs(attributeValue)
+                    outerHTML.push(makeHTMLAttribute(attributeName, attributeValue))
+                    break
+                default:
+                    outerHTML.push(makeHTMLAttribute(attributeName, attributeValue))
+             }
         }
         
-		return interpolate(tagName, innerHTML, outerHTML)
-	}
+        return interpolate(tagName, innerHTML, outerHTML)
+    }
 }
 
 /**
@@ -69,8 +69,8 @@ function makeStyleElement(template){
     // one weakness of using this shorthand for style elements is I skip support for attributes on the style tag -- type, media, title
     // so all I can say for now is if you want those things use a link element, for inline style this is all that's available.
     // Could make a distinciton between style elements with objects vs arrays as the cssSelector sets to allow this
- 	let [[tagName, cssSelectorSet]] = Object.entries(template)
- 	return interpolate(tagName, Array(makeCSSSelectorSet(cssSelectorSet)))
+     let [[tagName, cssSelectorSet]] = Object.entries(template)
+     return interpolate(tagName, Array(makeCSSSelectorSet(cssSelectorSet)))
 }
 
 /**
@@ -81,16 +81,16 @@ function makeStyleElement(template){
  * Handles media and other @ tags by recursing the rule set and enclosing it in brackets
  */
 function makeCSSSelectorSet(cssSelectorSet, seperator = '\n'){
-	if(Type.isArray(cssSelectorSet)){
-		return cssSelectorSet.map(makeCSSSelectorSet).join(seperator)
-	} else {
-		return Object.entries(cssSelectorSet).map(
-			([selector, ruleValuePair]) => 
-				selector[0] == '@'
-					? `${selector} {${makeCSSSelectorSet(ruleValuePair)}}`
-					: `${selector} {${makeCSSRuleValuePairs(ruleValuePair)}}`
-		).join(seperator)
-	}
+    if(Type.isArray(cssSelectorSet)){
+        return cssSelectorSet.map(makeCSSSelectorSet).join(seperator)
+    } else {
+        return Object.entries(cssSelectorSet).map(
+            ([selector, ruleValuePair]) => 
+                selector[0] == '@'
+                    ? `${selector} {${makeCSSSelectorSet(ruleValuePair)}}`
+                    : `${selector} {${makeCSSRuleValuePairs(ruleValuePair)}}`
+        ).join(seperator)
+    }
 }
 
 /**
@@ -99,10 +99,10 @@ function makeCSSSelectorSet(cssSelectorSet, seperator = '\n'){
  * @return {string} ex. `width: 100px; height: 50px;` ready to embed in an attribute or style tag.
  */
 function makeCSSRuleValuePairs(RuleValuePairs, seperator = ' '){
-	// assertSchema('CSSRuleValuePairs', RuleValuePairs)
-	return Object.entries(RuleValuePairs).map(([rule, value]) => 
-		`${rule}: ${Array.isArray(value) ? value.join('') : value};`
-	).join(seperator)
+    // assertSchema('CSSRuleValuePairs', RuleValuePairs)
+    return Object.entries(RuleValuePairs).map(([rule, value]) => 
+        `${rule}: ${Array.isArray(value) ? value.join('') : value};`
+    ).join(seperator)
 }
 
 /**
@@ -117,7 +117,7 @@ function makeHTMLAttribute(attrName, attrValue){
     attrValue = attrValue
         .replace('"', '&quot;')
         .replace('&', '&amp;')
-	return ` ${attrName}="${attrValue}"`
+    return ` ${attrName}="${attrValue}"`
 }
 
 /**
@@ -129,10 +129,10 @@ function makeHTMLAttribute(attrName, attrValue){
 function interpolate(tagName, innerHTML = [], outerHTML = []){
     innerHTML = Type.isArray(innerHTML) ? innerHTML.join('') : innerHTML // taking care not to add space between HTML tags
     outerHTML = Type.isArray(outerHTML) ? outerHTML.join('') : outerHTML // and attributes will have their own spaces embedded
-	// cant deal with arguments of improper type, will throw error calling join
-	if(empty_elements.includes(tagName)){
-		return `\n<${tagName}${outerHTML}>`
-	} else {
-		return `\n<${tagName}${outerHTML}>${innerHTML}</${tagName}>`
-	}
+    // cant deal with arguments of improper type, will throw error calling join
+    if(empty_elements.includes(tagName)){
+        return `\n<${tagName}${outerHTML}>`
+    } else {
+        return `\n<${tagName}${outerHTML}>${innerHTML}</${tagName}>`
+    }
 }
